0bc445fd0f16e100214ee2ff27936ceffbde7525
Adham Amr <104816206+Adham-amr-1@users.noreply.github.com>
2023-06-09 10:55:06 +0300

    Create #0.cpp

---
+++ #0.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void hashs();
+int main() {
+	hashs();
+	cout << "Hello" << endl;
+	hashs();
+	cout << "Byebye" << endl;
+	return 0;
+}
+void hashs() {
+	int i, k;
+	for (i = 0; i < 4; i++) {
+		for (k = 0; k < 4; k++) {
+			cout << "#";
+		}
+		cout << endl;
+	}
+}
